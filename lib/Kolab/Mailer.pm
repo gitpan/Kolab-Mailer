74bf8df5d4c5ba16e42f1658e50b0cd0d6b67037
Stephan Buys <sbproxy@icon.co.za>
2004-05-11 10:33:03 +0000

    Import of STEPHANB/Kolab-Mailer-1.01 from CPAN.
    gitpan-cpan-distribution: Kolab-Mailer
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         STEPHANB/Kolab-Mailer-1.01.tar.gz
    gitpan-cpan-author:       STEPHANB
    gitpan-cpan-maturity:     released

--- lib/Kolab/Mailer.pm
+++ lib/Kolab/Mailer.pm
@@ -40,10 +40,10 @@ our %EXPORT_TAGS = (
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = qw(
-    
+
 );
 
-our $VERSION = '0.9';
+our $VERSION = sprintf('%d.%02d', q$Revision: 1.1.1.1 $ =~ /(\d+)\.(\d+)/);
 
 sub sendMultipart
 {
